b46cba5b05080a6235a88830ef53421ab5548b48
Ruchir Bisht <ruchircr7@gmail.com>
2020-12-22 20:20:14 +0530

    Module 2 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+// 	return (
+// 		<div className='App'>
+// 			<h1>RUCHIR BISHT</h1>
+// 		</div>
+// 	);
+// }
+
+//class based component
+class App extends Component {
+	foo = (username) => {
+		return 'Hello ' + username;
+	};
+
+	render() {
+		const name = 'John Doe';
+		const loading = false;
+		const showName = false;
+		return (
+			<Fragment>
+				<h1>Github Finder</h1>
+				{loading ? <h4>Loading ... </h4> : <h3>{showName && this.foo(name)}</h3>}
+			</Fragment>
+		);
+	}
 }
 
 export default App;
